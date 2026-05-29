d5fbf786e86ee725a56eedcf8bb9452ba6aa5613
rasenrengan <pitbulltsport@gmail.com>
2026-05-29 03:02:20 +0300

    Update js/app.js

--- js/app.js
+++ js/app.js
@@ -273,7 +273,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // 3. Global Floating Action Button (Call Us)
     const callFAB = document.createElement('a');
-    callFAB.href = 'tel:+201200669461';
+    callFAB.href = 'tel:+201551552171';
     callFAB.className = 'floating-call-btn';
     callFAB.setAttribute('aria-label', 'Call Us');
     callFAB.innerHTML = `
